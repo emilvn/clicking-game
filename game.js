2043b53b5e79e91487eed3bc5cfb317666bd88f1
emilvn <emilvnielsen@hotmail.com>
2023-02-13 15:26:37 +0100

    made a clock

--- game.js
+++ game.js
@@ -1,25 +1,28 @@
 function healthBar(lives_left){
 const style = document.createElement('style');
 if (lives_left == 3){
-    style.textContent = `
-    #life_heart1, #life_heart2, #life_heart3 {
-        filter: grayscale(0);
-    }
-    `
+    return
 }
 else if (lives_left == 2){
     style.textContent = `
-    #life_heart1, #life_heart2 {
-        filter: grayscale(0);
+    #life_heart3 {
+        filter: grayscale(1);
     }
     `
 }
 else if (lives_left == 1){
     style.textContent = `
-    #life_heart1 {
-        filter: grayscale(0);
+    #life_heart3, #life_heart2 {
+        filter: grayscale(1);
     }
     `
+}
+else if (lives_left == 0) {
+    style.textContent = `
+    #life_heart3, #life_heart2, #life_heart1 {
+        filter: grayscale(1);
+    }
+    `;
 }
 else {
     return
